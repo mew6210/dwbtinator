d1a21fc5fcd40a5473a90f28a697a6d759974ea2
mew6210 <mew.62@wp.pl>
2025-09-24 18:21:36 +0200

    feat(core): basic process name extraction
    - gets current window handle, then gets process handle from it and parses path to this process to include only its name
    - edge cases:
    -- if extension is .EXE, it fails parsing
    --output is not always clean, for example "GithubDesktop", there should be a space between the words

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,15 @@
+use std::time;
+
+use crate::wintracker::windows::get_current_active_process_name;
+
+mod wintracker;
 fn main() {
-    println!("Hello, world!");
+    loop{
+        unsafe{
+            let title = get_current_active_process_name();
+            println!("{}",title);
+            std::thread::sleep(time::Duration::from_secs(5));
+        }
+    }
+
 }
---
+++ src/wintracker/windows.rs
@@ -0,0 +1,50 @@
+use winapi::{ctypes::c_void, shared::{minwindef::{DWORD, FALSE}, windef::HWND__}, um::{processthreadsapi::OpenProcess, psapi::GetProcessImageFileNameA, winnt::{PROCESS_QUERY_INFORMATION, PROCESS_VM_READ}, winuser::{GetForegroundWindow, GetWindowThreadProcessId}}};
+
+#[cfg(windows)] extern crate winapi;
+
+unsafe fn get_current_active_window_handle() -> *mut HWND__{
+    
+    unsafe{
+        let handle = GetForegroundWindow();
+        return handle;
+    }
+}
+unsafe fn get_process_name(handle:*mut c_void) -> String{
+
+    const BUFFER_SIZE:usize = 256;
+    let mut buffer: [i8;BUFFER_SIZE] =[0;BUFFER_SIZE];
+    unsafe{
+        let name_length = GetProcessImageFileNameA(handle, buffer.as_mut_ptr(), BUFFER_SIZE.try_into().unwrap());
+        let name = String::from_utf8_lossy(&buffer[..name_length.try_into().unwrap()].iter().map(|&c| c as u8 ).collect::<Vec<u8>>()).into_owned();
+        return name;
+    }
+
+}
+
+unsafe fn current_active_window_handle_to_process_handle(window_handle:*mut HWND__) -> *mut c_void{
+    unsafe{
+        let mut dw_pid: DWORD = 0;
+        GetWindowThreadProcessId(window_handle, &mut dw_pid);
+        OpenProcess(PROCESS_QUERY_INFORMATION | PROCESS_VM_READ, FALSE, dw_pid)
+
+    }
+}
+
+fn parse_process_name(name:&str)->Option<String>{
+
+    name.rsplit('\\').next().map(|name|{
+        name.strip_suffix(".exe").unwrap_or(name).to_string()
+    })
+}
+
+#[cfg(windows)]
+pub unsafe  fn get_current_active_process_name() -> String{
+    
+    unsafe{
+        let window_handle = get_current_active_window_handle();
+        let process_handle = current_active_window_handle_to_process_handle(window_handle);
+        let name = get_process_name(process_handle);
+        parse_process_name(&name).unwrap_or(name)
+        
+    }
+}
